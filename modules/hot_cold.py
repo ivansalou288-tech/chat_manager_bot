b4f31c6c60ebdfa870ab6d28a91781140ab74254
Ezhik <my_email@gmail.com>
2025-12-19 22:50:40 +0300

    добавлен чс

--- modules/hot_cold.py
+++ modules/hot_cold.py
@@ -1,6 +1,14 @@
+import html
+import sys
+import os
+import sqlite3
 import random
-from aiogram import types
-from aiogram.dispatcher import Dispatcher
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+from aiogram import types, Dispatcher
+from aiogram.dispatcher.filters import Text
+from aiogram.types import ContentType
+from main.config import main_path, chats, bot, dp
 
 # Dictionary to store the target number for each chat
 chat_targets = {}
@@ -11,6 +19,17 @@ def register_hot_cold_handlers(dp: Dispatcher):
     dp.register_message_handler(cancel_hot_cold, commands=['стоп-хг'], commands_prefix='!/.')
 
 async def start_hot_cold(message: types.Message):
+                
+    connection = sqlite3.connect(main_path, check_same_thread=False)
+    cursor = connection.cursor()
+    black_list=[]
+    blk = cursor.execute('SELECT user_id FROM black_list').fetchall()
+    for i in blk:
+        black_list.append(i[0])
+
+    if message.from_user.id in black_list:
+            await message.answer('В доступе отказано, ты в черном списке')
+            return
     """Start the Hot-Cold game by generating a random number."""
     chat_id = message.chat.id
     target_number = random.randint(1, 100)  # Bot picks a number between 1 and 100
