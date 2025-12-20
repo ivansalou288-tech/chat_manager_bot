6542fda2a687c478145ece27f84cfa6b69ee4747
Ноутбук Ezhik <ezhik@gmail.com>
2025-12-20 13:44:10 +0300

    Добавлена документация для админ бота

--- testbot.py
+++ testbot.py
@@ -1,9 +1,9 @@
 
 import markdown
-with open('docs/USER_GUIDE.md', 'r', encoding='utf-8') as f:
+with open('docs/admin_guide.md', 'r', encoding='utf-8') as f:
     text = f.read()
     html = markdown.markdown(text)
 
 
-with open('docs/USER_GUIDE.html', 'w', encoding='utf-8') as f:
+with open('docs/admin_guide.html', 'w', encoding='utf-8') as f:
     f.write(html)
